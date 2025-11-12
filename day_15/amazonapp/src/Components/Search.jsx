b3be562f96da9ad444d3a96a173e5c5733ca6dbb
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-11-12 09:56:06 +0530

    vite app with routing

---
+++ day_15/amazonapp/src/Components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search = ({searchHandler}) => {
+    return (
+        <div className="search">
+            <input type="text" name="search" id="search"
+                onChange={(e) => searchHandler(e.target.value)} />
+        </div>
+    )
+}
+
+export default Search
